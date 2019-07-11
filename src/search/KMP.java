b58689d3a6324d1116b3c5f4a70ef0bb27fc8821
arthur <arthur.choi@within.co.kr>
2019-07-11 18:00:49 +0900

    KMP init

---
+++ src/search/KMP.java
@@ -0,0 +1,14 @@
+package search;
+
+/**
+ * https://bowbowbow.tistory.com/6
+ */
+public class KMP {
+    public static void main(String[] args){
+        String inputStr = "ABCDABCDABEE";
+        String pattern = "ABCDABE";
+
+        char[] sourceChar = inputStr.toCharArray();
+        char[] patternChar = pattern.toCharArray();
+    }
+}
