919277b0cf5ab29d16a0efc683dd0d432fde6e14
arthur <arthur.choi@within.co.kr>
2019-10-18 21:33:27 +0900

    배민 코딩테스트 정리

---
+++ src/coding_test/woowa/Question04.java
@@ -0,0 +1,25 @@
+package coding_test.woowa;
+
+import java.util.Arrays;
+
+public class Question04 {
+    public static void main(String[] args) {
+        Question04 question = new Question04();
+        System.out.println(question.solution(new int[]{1, 2, 3, 13}, new int[]{10, 11, 12, 9, 13}));
+    }
+
+    int solution(int[] A, int[] B) {
+        int n = A.length;
+        int m = B.length;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int i = 0;
+        for (int k = n-1; k >= 0; k--) { // <--
+            while (i < m -1 && B[i] < A[k]) // <--
+                i += 1;
+            if (A[k] == B[i])
+                return A[k];
+        }
+        return -1;
+    }
+}
